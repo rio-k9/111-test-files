cbdb5e9a75706de735114990aaab875db7a54d7c
Rio Knightley <rio.knightley@madetech.com>
2022-07-12 08:04:26 +0100

    Update cert extensions

--- itk/test/test-itk.sh
+++ itk/test/test-itk.sh
@@ -1,1 +1,3 @@
-curl --request POST http://13.40.139.175/api/report --data @primary-emergency-itk-request.xml -H "Content-Type: application/xml"
+curl --insecure --request POST https://13.40.139.175/report --data @primary-emergency-itk-request.xml -H "Content-Type: application/xml"
+
+curl --cert certs/client_pkcs.p12 --cert-type p12 --pass logitech --cacert certs/client_public.cer  --request POST https://13.40.139.175/report --data @test/primary-emergency-itk-request.xml -H "Content-Type: application/xml"
